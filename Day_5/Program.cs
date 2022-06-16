59bacf8a34e23c9ed2718c4ceb9d8e820f2bdff2
your Name <mohamedabdulayubm@gmail.com>
2022-06-16 16:14:29 +0530

    EvenOdd

--- Day_5/Program.cs
+++ Day_5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = A + B;
+Console.WriteLine(C);
+if (C%2==0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+    Console.WriteLine("Odd");
+}
