f7a0679de11a4e199947ba5cdef265b4f6294d09
your Name <mohamedabdulayubm@gmail.com>
2022-06-16 12:57:11 +0530

    Headtails

--- Day_5/Program.cs
+++ Day_5/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Heads = 0;
+int Tails = 0;
+double Hp = 0;
+double Tp = 0;
+
+Random hd = new Random();
+int Flip = hd.Next(0,2);
+int i = 0;
+while ( i < 10)
+{
+
+
+    if (Flip < 0.5)
+    {
+        Console.WriteLine("Its Tails");
+        Tails++;
+         i++;
+    }
+    else
+    {
+        Console.WriteLine("Its Heads");
+        Heads++;
+        i++;
+    }
+}
+Hp = Heads / .1;
+Tp = Tails / .1;
+Console.WriteLine("Heads percentage :" + Hp);
+Console.WriteLine("Tails percentage :" + Tp);
+
+
+      
