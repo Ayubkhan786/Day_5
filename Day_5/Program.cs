f1fd3f5ce12a2eeaa76d9f40323572082adede0b
your Name <mohamedabdulayubm@gmail.com>
2022-06-16 12:18:41 +0530

    leapyear

--- Day_5/Program.cs
+++ Day_5/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int year= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Given year =" +year);
+if (DateTime.IsLeapYear(year))
+{
+    Console.WriteLine(year + ": Is leap year");
+}
+else
+{
+    Console.WriteLine(year + ": Is not a leap year");
+}
