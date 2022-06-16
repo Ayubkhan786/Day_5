c0fd77e236225c0f2086f5a423011d425cf3387b
your Name <mohamedabdulayubm@gmail.com>
2022-06-16 15:43:57 +0530

    Swap

--- Day_5/Program.cs
+++ Day_5/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int firstNum = 100;
+int secNum = 50;
+
+firstNum *=secNum; 
+secNum = firstNum / secNum; 
+firstNum /= secNum; 
+
+Console.WriteLine("SwapValue of firstNum :" + firstNum);
+Console.WriteLine("SwapValue of secNum:" + secNum);
