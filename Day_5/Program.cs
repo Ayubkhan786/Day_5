1c355d00c8099caede5eea8313428ba55e8a855a
your Name <mohamedabdulayubm@gmail.com>
2022-06-17 21:25:35 +0530

    Prime

--- Day_5/Program.cs
+++ Day_5/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Master");
+﻿
+Console.WriteLine("Write a Number");
+int P = Convert.ToInt32(Console.ReadLine());
+int N;
+
+for ( N=1;(N*N <= P);N++ )
+{
+    if((P % N ==0))
+    
+    Console.WriteLine(N+ "is PrimeFactor ");
+   
+}
