0ed8a59efe70255d58471c9415a9811f126133a7
your Name <mohamedabdulayubm@gmail.com>
2022-06-18 15:58:27 +0530

    Harmonic

--- Day_5/Program.cs
+++ Day_5/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Master");
+﻿int i,n;
+n = 50;
+for (i=1; i<=n; i++)
+{
+    Console.Write("1/{0} + ",i);
+   
+}
+if (n == 50)
+{
+    Console.WriteLine("The nth Harmonic Number : 1/{0} ", n);
+}
